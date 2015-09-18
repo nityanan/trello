7f606ae9cee97c6acc806d20b7104ecbc875422f
rahul kamboj <kamo.rahul@gmail.com>
2015-09-19 01:06:54 +0530

    2files added

---
+++ practiceakki.js
@@ -0,0 +1,50 @@
+var x={akshay:[{company:[{current:"daffodil software",address:"Hisar"},{previous:"HCL Technologies",address:"Noida"	}],	hobbies:["passionate","magnanimous",{qualifications:[{professional:[{internship:"Nucleus",project:["HRMS","EMI Calculator", "Virtual Classroom"]},personal:[{non-it:["badminton","cricket"],achievements:[{trophy:"lan gaming",certi:["web master","badminton","code crunch"]}]}]]}]}]}]}
+var x={akshay:[{company:[{current:"daffodil software",add{ress:"Hisar"},{previous:"HCL Technologies",address:"Noida"}],hobbies:["passionate","magnanimous",{qualifications:[{professional:[{internship:"Nucleus",project:["HRMS","EMI Calculator", "Virtual Classroom"]},personal:[{non_it:["badminton","cricket"],achievements:[{trophy:"lan gaming",certi:["web master","badminton","code crunch"]}]}]]}]}]}]}
+var x={akshay:[{company:[{current:"daffodil software",address:"Hisar"},{previous:"HCL Technologies",address:"Noida"}],hobbies:["passionate","magnanimous",{qualifications:[{professional:[{internship:"Nucleus",project:["HRMS","EMI Calculator", "VirtualClassroom"]},personal:[{nonit:["badminton","cricket"],achievements:[{trophy:"lan gaming",certi:["web master","badminton","code crunch"]}]}]]}]}]}]}
+var x={akshay:[{company:[{current:"daffodil software",address:"Hisar"},{previous:"HCL Technologies",address:"Noida"}],hobbies:["passionate","magnanimous",{qualifications:[{professional:[{internship:"Nucleus",project:["HRMS","EMI Calculator", "Virtual Classroom"]}],personal:[{nonit:["badminton","cricket"],achievements:[{trophy:"lan gaming",certi:["web master","badminton","code crunch"]}]}]}]}]}]}
+
+
+
+a.b[0].c[2].d.e[1][3][2].f.g[0].h[1].i[1][2].j
+a.b[0].c[2].d.e[1][3][2].f.g[0].h[1].i[1][2].j
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var a = {b:[{c:[{},{},{d:{e:[{},[{},{},{},[{},{},{f:{g:[{h:[{},{i:[{},[{},{},{j:"nitya"}]]}]}]}}]]]}}]}]}
+var a = {b:[{c:["","",{d:{e:["",["","","",["","",{f:{g:[{h:["",{i:["",["","",{j:"nitya"}]]}]}]}}]]]}}]}]}
+var a = {b:[{c:["","",{d:{e:["",["","","",["","",{f:{g:[{h:["",{i:["",["","",{j:"nitya"}]]}]}]}}]]]}}]}]}
